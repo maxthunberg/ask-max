1266a0ff51f746ff484b44d7f5f4aa7e44de1063
Max Thunberg <max@maxthunberg.com>
2025-12-01 00:47:41 +0100

    New try

--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import { Send, Info } from 'lucide-react';
-import { sendChatMessage, type ChatMessage } from '../utils/chat-api.ts';
+import { sendChatMessage, type ChatMessage } from '../utils/chat-api';
 
 interface ChatInterfaceProps {
   onInfoClick?: () => void;
