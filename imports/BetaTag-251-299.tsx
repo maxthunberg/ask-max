9e59eff29f07d9507caf7ec95977f8fc50da7f39
Max Thunberg <max@maxthunberg.com>
2025-12-04 18:11:21 +0100

    Fix beta tag wrong font

--- imports/BetaTag-251-299.tsx
+++ imports/BetaTag-251-299.tsx
@@ -1,7 +1,7 @@
 export default function BetaTag() {
   return (
     <div className="bg-[rgba(235,212,33,0.15)] box-border content-stretch flex gap-[10px] items-center justify-center px-[4px] py-0 relative rounded-[4px] shrink-0" data-name="Beta tag">
-      <p className="font-['Figtree:Regular',sans-serif] font-normal leading-[20px] relative shrink-0 text-[#ebd421] text-[14px] text-nowrap whitespace-pre">Beta</p>
+      <p className="leading-[20px] relative shrink-0 text-[#ebd421] text-[14px] text-nowrap whitespace-pre" style={{ fontFamily: '"Figtree:Regular", sans-serif', fontWeight: 400, fontStyle: 'normal' }}>Beta</p>
     </div>
   );
 }
